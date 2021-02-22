cde407db70fb3ea70abd5fafb80e1a269118cef9
Seungwook Han <sh3264@coumbia.edu>
2021-02-22 20:11:59 +0900

    Fixed logging

--- tools/logger.py
+++ tools/logger.py
@@ -33,7 +33,7 @@ class Logger(object):
                 self.counter[key] += 1
 
             if self.writer:
-                self.writer.add_scalar(key, value.mean(), self.counter[key])
+                self.writer.add_scalar(key, value.mean() if isinstance(value, Tensor) else value, self.counter[key])
 
 
         if self.plotter: 
