ae62077d87a9ea05327ef50bff38d2174d9d0aeb
Seungwook Han <sh3264@coumbia.edu>
2021-02-25 18:54:06 +0900

    Fixed fetching pretrained script

--- tools/fetch_pretrained.sh
+++ tools/fetch_pretrained.sh
@@ -4,21 +4,21 @@
 cd models/backbones/state_dicts/
 
 wget http://shape2prog.csail.mit.edu/repo/wrn_40_2_vanilla/ckpt_epoch_240.pth
-mv ckpt_epoch_240.pth wrn_40_2.pth
+mv ckpt_epoch_240.pth cifar100/wrn_40_2.pth
 
 wget http://shape2prog.csail.mit.edu/repo/resnet56_vanilla/ckpt_epoch_240.pth
-mv ckpt_epoch_240.pth resnet56.pth
+mv ckpt_epoch_240.pth cifar100/resnet56.pth
 
 wget http://shape2prog.csail.mit.edu/repo/resnet110_vanilla/ckpt_epoch_240.pth
-mv ckpt_epoch_240.pth resnet110.pth
+mv ckpt_epoch_240.pth cifar100/resnet110.pth
 
 wget http://shape2prog.csail.mit.edu/repo/resnet32x4_vanilla/ckpt_epoch_240.pth
-mv ckpt_epoch_240.pth resnet32x4.pth
+mv ckpt_epoch_240.pth cifar100/resnet32x4.pth
 
 wget http://shape2prog.csail.mit.edu/repo/vgg13_vanilla/ckpt_epoch_240.pth
-mv ckpt_epoch_240.pth vgg13.pth/
+mv ckpt_epoch_240.pth cifar100/vgg13.pth
 
 wget http://shape2prog.csail.mit.edu/repo/ResNet50_vanilla/ckpt_epoch_240.pth
-mv ckpt_epoch_240.pth resnet50.pth
+mv ckpt_epoch_240.pth cifar100/resnet50.pth
 
 cd ../../../
