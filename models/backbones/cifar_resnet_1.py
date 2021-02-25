b95fcaf971eb95a025bcc6bb01ad8ff5490a9bcd
Seungwook Han <sh3264@coumbia.edu>
2021-02-25 18:47:44 +0900

    Specified cifar10 weights for this arch

--- models/backbones/cifar_resnet_1.py
+++ models/backbones/cifar_resnet_1.py
@@ -201,7 +201,7 @@ def _resnet(arch, block, layers, pretrained, progress, device, **kwargs):
     model = ResNet(block, layers, **kwargs)
     if pretrained:
         script_dir = os.path.dirname(__file__)
-        state_dict = torch.load(script_dir + '/state_dicts/'+arch+'.pt', map_location=device)
+        state_dict = torch.load(script_dir + '/state_dicts/cifar10/'+arch+'.pt', map_location=device)
         model.load_state_dict(state_dict)
     return model
 
