68cab3b21adf3e097c69d70582fb6f69a7cdd60f
Seungwook Han <sh3264@coumbia.edu>
2021-05-03 20:14:05 -0400

    Fixed boolean comp

--- tools/utils.py
+++ tools/utils.py
@@ -2,6 +2,6 @@ import torch
 import IPython
 
 def check_nan_inf(loss):
-    if torch.isnan(loss) or torch.isinf(loss):
+    if torch.isnan(loss).any() or torch.isinf(loss).any():
         print("Loss is NaN or inf")
         IPython.embed()
