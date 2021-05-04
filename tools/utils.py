f59d6dfdda1747766032a16c214549f49b887c56
Seungwook Han <sh3264@columbia.edu>
2021-05-04 20:34:42 +0000

    Fixed yaml req and changed to set_trace

--- tools/utils.py
+++ tools/utils.py
@@ -1,7 +1,9 @@
 import torch
 import IPython
 
+from pdb import set_trace
+
 def check_nan_inf(loss):
     if torch.isnan(loss).any() or torch.isinf(loss).any():
         print("Loss is NaN or inf")
-        IPython.embed()
+        set_trace()
