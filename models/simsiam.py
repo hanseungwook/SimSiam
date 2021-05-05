Fixed creating cov matrix from variance vec

models/simsiam.py
@@ -177,8 +177,8 @@ class SimSiamNoSG(nn.Module):
         z2_mu_norm = (z2_mu - z2_mu.mean(0)) / z2_mu.std(0)
 
         # Pytorch internal method of KL
-        z1_dist = torch.distributions.multivariate_normal.MultivariateNormal(loc=z1_mu_norm, covariance_matrix=torch.diag(z1_var))
-        z2_dist = torch.distributions.multivariate_normal.MultivariateNormal(loc=z2_mu_norm, covariance_matrix=torch.diag(z2_var))
+        z1_dist = torch.distributions.multivariate_normal.MultivariateNormal(loc=z1_mu_norm, covariance_matrix=torch.diag_embed(z1_var))
+        z2_dist = torch.distributions.multivariate_normal.MultivariateNormal(loc=z2_mu_norm, covariance_matrix=torch.diag_embed(z2_var))
 
         z1_kl = torch.distributions.kl.kl_divergence(z1_dist, z2_dist.detach())
         z2_kl = torch.distributions.kl.kl_divergence(z2_dist, z1_dist.detach())